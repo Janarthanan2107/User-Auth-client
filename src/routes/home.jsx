2b16fa4de53f69ec291e03fe21791c54daeb8c8f
Janarthanan2107 <janarthanan.v2107@gmail.com>
2024-02-22 17:24:21 +0530

    greeting message

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,5 +1,15 @@
 const Home = () => {
-  return <div>Guest User</div>;
+  const user = JSON.parse(localStorage.getItem("user"));
+  return (
+    <div
+      className="flex justify-center items-center h-screen"
+      style={{ height: "calc(100vh - 80px)" }}
+    >
+      <div className="bg-blue-400 text-black font-semibold rounded-xl p-8">
+        Welcome {user ? user.username : "Guest"}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
